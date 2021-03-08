9d95536f14cdd21c13122dbb5ebe9e5e9438a802
Buck Tower <buck@bucktower.net>
2021-03-07 20:55:19 -0500

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,82 @@
-import Head from 'next/head'
+import Head from "next/head";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Swashbuckle Studios</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">Welcome to Swashbuckle Studios</h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Welcome to the chaos and creation of{" "}
+          <a href="https://bucktower.net" target="_blank">
+            Buck Tower
+          </a>
+          .
+          <br />
+          This is the hub of all of my personal projects for all my different
+          hobbies that I’d like to share
         </p>
 
         <div className="grid">
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Swashbuckle Records &rarr;</h3>
+            <p>
+              I like music like a dog loves running. I make it for my own
+              pleasure, but I like sharing it with friends.
+            </p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
+            <h3>Swashbuckle Software &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
+              Sometimes, it's great to try out a software project the way I want
+              to architect it from the ground up
             </p>
           </a>
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip id={`tooltip-stories`}>
+                Nope. <strong>Not yet</strong>...
+              </Tooltip>
+            }
+          >
+            <span className="card card-disabled">
+              <h3>Swashbuckle Stories</h3>
+              <p>
+                I enjoy reading and writing stories. It’d be cool to one day put
+                them into some poetry, books, or film.
+              </p>
+            </span>
+          </OverlayTrigger>
+          <OverlayTrigger
+            placement="top"
+            overlay={
+              <Tooltip id={`tooltip-stories`}>
+                Big sad. <strong>Someday</strong>...
+              </Tooltip>
+            }
+          >
+            <span className="card card-disabled">
+              <h3>Swashbuckle Kitchen</h3>
+              <p>
+                Someday, I’d like to compile a big cookbook, and maybe some kind
+                of cooking Youtube channel.
+              </p>
+            </span>
+          </OverlayTrigger>
         </div>
       </main>
 
       <footer>
-        <a
-          href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
+        <a href="https://twitter.com/bucktower" target="_blank">
+          Message me on Twitter @bucktower &rarr;
         </a>
       </footer>
 
@@ -101,14 +123,14 @@ export default function Home() {
           text-decoration: none;
         }
 
-        .title a {
+        .description a {
           color: #0070f3;
           text-decoration: none;
         }
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
+        .description a:hover,
+        .description a:focus,
+        .description a:active {
           text-decoration: underline;
         }
 
@@ -166,6 +188,13 @@ export default function Home() {
           border-color: #0070f3;
         }
 
+        .card-disabled:hover,
+        .card-disabled:focus,
+        .card-disabled:active {
+          color: grey;
+          border-color: grey;
+        }
+
         .card h3 {
           margin: 0 0 1rem 0;
           font-size: 1.5rem;
@@ -200,5 +229,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
